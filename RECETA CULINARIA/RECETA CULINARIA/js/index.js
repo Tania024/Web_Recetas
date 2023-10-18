fe5ad10453aadeb9faa5fc7f6a6dee8847ae222e
Tania024 <84852408+Tania024@users.noreply.github.com>
2023-10-17 23:54:25 -0500

    formulario
    terminado

---
+++ RECETA CULINARIA/RECETA CULINARIA/js/index.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el formulario y los campos de entrada
+    const form = document.getElementById("survey-form");
+    const nombreInput = document.getElementById("nombre");
+    const ingredientesInput = document.getElementById("ingredientes");
+    const pasosInput = document.getElementById("pasos");
+  
+    // Agregar un controlador de eventos para enviar el formulario
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      // Obtener los valores de los campos de entrada
+      const nombre = nombreInput.value;
+      const ingredientes = ingredientesInput.value;
+      const pasos = pasosInput.value;
+  
+      // Validar que los campos no estén vacíos
+      if (nombre.trim() === "" || ingredientes.trim() === "" || pasos.trim() === "" ) {
+        alert("Por favor, completa todos los campos.");
+        return;
+      }
+  
+      // Guardar los datos en el Local Storage
+      saveDataToLocalStorage(nombre, ingredientes, pasos);
+  
+      // Reiniciar los campos del formulario
+      form.reset();
+    });
+  
+    // Función para guardar los datos en el Local Storage
+    function saveDataToLocalStorage(nombre, ingredientes,pasos) {
+      // Obtener los datos anteriores del Local Storage
+      let storedData = localStorage.getItem("surveyData");
+      if (storedData === null) {
+        storedData = [];
+      } else {
+        storedData = JSON.parse(storedData);
+      }
+  
+      // Agregar los nuevos datos a la lista
+      storedData.push({ nombre, ingredientes, pasos });
+  
+      // Guardar la lista actualizada en el Local Storage
+      localStorage.setItem("surveyData", JSON.stringify(storedData));
+    }
+  });
+
+
+  
+  
